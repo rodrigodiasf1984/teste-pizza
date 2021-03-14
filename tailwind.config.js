47b86c54b9f7e04d33d16cdcc5a9852a23c126ee
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-03-14 21:54:48 +0100

    refactor(config of the project): config
    added the necessary libs, configs to the project

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      maxHeight:{
+        '680px': '680px'
+      },
+      colors:{
+        red:{
+          custom: '#d1411e'
+        }
+      },
+      backgroundImage:{
+        'pizza': "url('/assets/pizza.svg')",
+      },
+      screens: {
+        xxs: { min: "280px", max: "319px" },
+        xs: { min: "320px", max: "540px" },
+      },
+      fontFamily: {
+        roboto: ['Roboto']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
